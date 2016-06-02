858ed3eea496b2656365e067f397f57608ee5f21
Kevin VanEvery and Sky Rousse <kevin@>
2016-06-02 14:16:03 -0700

    insert js for selecting operator

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,39 @@
-/*This file is for your custom js.  All yours*/
+//Business logic
+var add = function(number1, number2) {
+	return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+	return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+	return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+	return number1 / number2;
+};
+//user interface logic
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+
+    event.preventDefault(); //prevent form submit
+    //collect user input
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    //calculate function required
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
+  });
+});
